9b327edbd21cb5b6b2dfa5f5a746595d6d6fc771
dfry-lhzn <dfry@lhzn.io>
2026-01-18 08:28:58 -0500

    chore: bump version to 0.5.0

--- kanoa/__init__.py
+++ kanoa/__init__.py
@@ -2,7 +2,7 @@
 kanoa: AI-powered interpretation of data science outputs.
 """
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
 
 from .config import options
 from .core.interpreter import AnalyticsInterpreter, supported_backends
